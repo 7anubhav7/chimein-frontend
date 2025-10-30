5f5bfe00f4de6e3e4844411f15d0f82dda7af1e3
anubhav <anubhavhajela37@gmail.com>
2025-10-30 20:09:37 +0530

    Completed terraform files

--- infrastructure/1-variables.tf
+++ infrastructure/1-variables.tf
@@ -21,40 +21,40 @@ variable "project" {
 variable "main_client_app_domain" {
   type = string
   description = "Main client app domain"
-  default = "dev.chimein" #purchase domain
+  default = "app.chimein.ink"
 }
 
 variable "dev_client_app_domain" {
   type = string
   description = "Dev client app domain"
-  default = "dev.chimein" #purchase domain
+  default = "app.dev.chimein.ink"
 }
 
 variable "custom_error_response" {
   type = list(object({
-    error_caching_min_ttl = string
-    error_code = string
-    response_code = string
+    error_caching_min_ttl = number
+    error_code = number
+    response_code = number
     response_page_path = string
   }))
   description = "List of one or more custom error response element maps"
   default = [
   {
-    error_caching_min_ttl = "10"
-    error_code = "400"
-    response_code = "400"
+    error_caching_min_ttl = 10
+    error_code = 400
+    response_code = 400
     response_page_path = "/index.html"
   },
   {
-    error_caching_min_ttl = "10"
-    error_code = "403"
-    response_code = "403"
+    error_caching_min_ttl = 10
+    error_code = 403
+    response_code = 403
     response_page_path = "/index.html"
   },
   {
-    error_caching_min_ttl = "10"
-    error_code = "404"
-    response_code = "404"
+    error_caching_min_ttl = 10
+    error_code = 403
+    response_code = 403
     response_page_path = "/index.html"
   },
 
